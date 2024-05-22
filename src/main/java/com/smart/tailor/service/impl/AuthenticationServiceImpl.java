702144ad35733fa3891506b8c85d13a754750c85
hoangminh-tran <90591751+hoangminh-tran@users.noreply.github.com>
2024-05-22 14:14:21 +0700

    [BE0006] Create Auth Service Login GG FB JWT

---
+++ src/main/java/com/smart/tailor/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,101 @@
+package com.smart.tailor.service.impl;
+
+import com.smart.tailor.entities.Token;
+import com.smart.tailor.entities.User;
+import com.smart.tailor.enums.Provider;
+import com.smart.tailor.enums.TokenType;
+import com.smart.tailor.service.AuthenticationService;
+import com.smart.tailor.service.JwtService;
+import com.smart.tailor.service.TokenService;
+import com.smart.tailor.service.UserService;
+import com.smart.tailor.utils.request.AuthenticationRequest;
+import com.smart.tailor.utils.request.UserRequest;
+import com.smart.tailor.utils.response.AuthenticationResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final TokenService tokenService;
+    @Override
+    public AuthenticationResponse register(UserRequest userRequest) {
+        userRequest.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        var user = userService.registerNewUsers(userRequest);
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse
+                .builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .user(userService.convertToUserResponse(user))
+                .build();
+    }
+
+    @Override
+    public AuthenticationResponse login(AuthenticationRequest authenticationRequest) {
+        try {
+            if (authenticationRequest.getProvider() != Provider.GOOGLE) {
+                if (authenticationRequest.getPassword().isBlank() || authenticationRequest.getPassword().isEmpty() ||
+                        authenticationRequest.getEmail().isEmpty() || authenticationRequest.getEmail().isBlank()) {
+                    throw new Exception("MISSING ARGUMENT");
+                }
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                authenticationRequest.getEmail(),
+                                authenticationRequest.getPassword()
+                        )
+                );
+            }
+            var user = userService.getUserByEmail(authenticationRequest.getEmail());
+            var jwtToken = jwtService.generateToken(user);
+            var refreshToken = jwtService.generateRefreshToken(user);
+            revokeAllUserTokens(user);
+            saveUserToken(user, jwtToken);
+            return AuthenticationResponse
+                    .builder()
+                    .accessToken(jwtToken)
+                    .refreshToken(refreshToken)
+                    .user(userService.convertToUserResponse(user))
+                    .build();
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenService.saveToken(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenService.findAllValidTokenByUser(user.getUserID());
+        if (validUserTokens.isEmpty())
+            return;
+        tokenService.revokeAllUserTokens(validUserTokens);
+    }
+
+    @Override
+    public AuthenticationResponse refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        return null;
+    }
+}
