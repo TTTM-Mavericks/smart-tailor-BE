cf8af2e8c8082fbcc033c5b0402104bc621b4b7a
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-07-10 15:33:44 +0700

    update order mapper

--- src/main/java/com/smart/tailor/mapper/OrderMapper.java
+++ src/main/java/com/smart/tailor/mapper/OrderMapper.java
@@ -3,8 +3,10 @@ package com.smart.tailor.mapper;
 import com.smart.tailor.entities.Order;
 import com.smart.tailor.utils.response.OrderResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+    @Mapping(source = "order.orderType", target = "orderType")
     OrderResponse mapToOrderResponse(Order order);
 }
