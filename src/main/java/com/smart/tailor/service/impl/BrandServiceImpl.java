4b5f0b53a7a9c917241f5fd761cb9daeefc83083
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-07-15 11:25:48 +0700

    [BE0016] crud brand

--- src/main/java/com/smart/tailor/service/impl/BrandServiceImpl.java
+++ src/main/java/com/smart/tailor/service/impl/BrandServiceImpl.java
@@ -1,11 +1,13 @@
 package com.smart.tailor.service.impl;
 
+import com.smart.tailor.constant.ErrorConstant;
 import com.smart.tailor.constant.MessageConstant;
 import com.smart.tailor.entities.Brand;
 import com.smart.tailor.entities.User;
 import com.smart.tailor.enums.BrandStatus;
 import com.smart.tailor.enums.Provider;
 import com.smart.tailor.enums.UserStatus;
+import com.smart.tailor.exception.BadRequestException;
 import com.smart.tailor.mapper.UserMapper;
 import com.smart.tailor.repository.BrandRepository;
 import com.smart.tailor.service.BrandService;
@@ -60,7 +62,12 @@ public class BrandServiceImpl implements BrandService {
         try {
             if (brandRequest.getEmail() != null && !brandRequest.getEmail().isEmpty()) {
                 var user = userService.getUserByEmail(brandRequest.getEmail().trim());
-                if (user != null) {
+                if (user == null) {
+                    throw new BadRequestException(MessageConstant.CAN_NOT_FIND_BRAND);
+                }
+                if (user.getUserStatus().equals(UserStatus.INACTIVE)) {
+                    throw new BadRequestException(ErrorConstant.ACCOUNT_NOT_VERIFIED.getMessage());
+                } else {
                     savedBrand = brandRepository.save(
                             Brand.builder()
                                     .user(user)
@@ -90,35 +97,32 @@ public class BrandServiceImpl implements BrandService {
             Provider provider = userRequest.getProvider() != null ? userRequest.getProvider() : Provider.LOCAL;
             userRequest.setProvider(provider);
 
-            if (provider == Provider.LOCAL) {
-                // check if Object exist in HashMap
-                UserRequest checkUserRequest = (UserRequest) storageObject.get(userRequest.getEmail());
-                if (checkUserRequest != null) {
-                    String oldToken = verifyAccount.get(userRequest.getEmail());
-                    LocalDateTime expiredTime = LocalDateTime.parse(expiredTimeLink.get(userRequest.getEmail() + " expiredTime"));
-                    verifyAccount.remove(oldToken);
-                    expiredTimeLink.remove(expiredTime);
-                    storageObject.remove(userRequest.getEmail());
-                }
 
-                // Store Object Class to HashMap
-                storageObject.put(userRequest.getEmail(), userRequest);
-
-                String token = UUID.randomUUID().toString();
-                verifyAccount.put(userRequest.getEmail(), token);
-
-                LocalDateTime now = LocalDateTime.now();
-                LocalDateTime expiredLinkVerify = now.plusMinutes(5);
-                expiredTimeLink.put(userRequest.getEmail() + " expiredTime", expiredLinkVerify.toString());
-
-                logger.info("Before Mail Email : {}, token : {}", userRequest.getEmail(), verifyAccount.get(userRequest.getEmail()));
-
-                String verificationUrl = "https://be.mavericks-tttm.studio/api/v1/brand/verify" + "?email=" + userRequest.getEmail() + "&token=" + token;
-
-                String emailText = "<!DOCTYPE html>" + "<html>" + "<head>" + "    <meta charset='UTF-8'>" + "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>" + "    <title>Account Verification</title>" + "    <style>" + "        body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; }" + "        .container { width: 100%; padding: 20px; }" + "        .content { background-color: #ffffff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }" + "        .header { font-size: 24px; font-weight: bold; color: #333333; }" + "        .message { font-size: 16px; color: #555555; }" + "        .button { display: inline-block; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #4CAF50; text-align: center; text-decoration: none; border-radius: 5px; margin-top: 20px; }" + "    </style>" + "</head>" + "<body>" + "    <div class='container'>" + "        <div class='content'>" + "            <div class='header'>Verify Your Account</div>" + "            <div class='message'>Hi " + userRequest.getEmail() + ",</div>" + "            <div class='message'>Thank you for registering. To complete your registration, please verify your email by clicking the button below.</div>" + "            <a href='" + verificationUrl + "' class='button'>Verify Account</a>" + "            <div class='message'>If you did not register for an account, please ignore this email.</div>" + "        </div>" + "    </div>" + "</body>" + "</html>";
-                emailSenderService.sendEmail(userRequest.getEmail(), "Account Verification", emailText);
-                return new UserResponse();
+            UserRequest checkUserRequest = (UserRequest) storageObject.get(userRequest.getEmail());
+            if (checkUserRequest != null) {
+                String oldToken = verifyAccount.get(userRequest.getEmail());
+                LocalDateTime expiredTime = LocalDateTime.parse(expiredTimeLink.get(userRequest.getEmail() + " expiredTime"));
+                verifyAccount.remove(oldToken);
+                expiredTimeLink.remove(expiredTime);
+                storageObject.remove(userRequest.getEmail());
             }
+
+            // Store Object Class to HashMap
+            storageObject.put(userRequest.getEmail(), userRequest);
+
+            String token = UUID.randomUUID().toString();
+            verifyAccount.put(userRequest.getEmail(), token);
+
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime expiredLinkVerify = now.plusMinutes(5);
+            expiredTimeLink.put(userRequest.getEmail() + " expiredTime", expiredLinkVerify.toString());
+
+            logger.info("Before Mail Email : {}, token : {}", userRequest.getEmail(), verifyAccount.get(userRequest.getEmail()));
+
+            String verificationUrl = "https://be.mavericks-tttm.studio/api/v1/brand/verify" + "?email=" + userRequest.getEmail() + "&token=" + token;
+
+            String emailText = "<!DOCTYPE html>" + "<html>" + "<head>" + "    <meta charset='UTF-8'>" + "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>" + "    <title>Account Verification</title>" + "    <style>" + "        body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; }" + "        .container { width: 100%; padding: 20px; }" + "        .content { background-color: #ffffff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }" + "        .header { font-size: 24px; font-weight: bold; color: #333333; }" + "        .message { font-size: 16px; color: #555555; }" + "        .button { display: inline-block; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #4CAF50; text-align: center; text-decoration: none; border-radius: 5px; margin-top: 20px; }" + "    </style>" + "</head>" + "<body>" + "    <div class='container'>" + "        <div class='content'>" + "            <div class='header'>Verify Your Account</div>" + "            <div class='message'>Hi " + userRequest.getEmail() + ",</div>" + "            <div class='message'>Thank you for registering. To complete your registration, please verify your email by clicking the button below.</div>" + "            <a href='" + verificationUrl + "' class='button'>Verify Account</a>" + "            <div class='message'>If you did not register for an account, please ignore this email.</div>" + "        </div>" + "    </div>" + "</body>" + "</html>";
+            emailSenderService.sendEmail(userRequest.getEmail(), "Account Verification", emailText);
             var user = User.builder().email(userRequest.getEmail()).password(passwordEncoder.encode(userRequest.getPassword())).userStatus(UserStatus.ACTIVE).fullName(userRequest.getFullName()).roles(roleService.findRoleByRoleName("BRAND").get()).phoneNumber(userRequest.getPhoneNumber()).imageUrl(userRequest.getImageUrl()).language(userRequest.getLanguage()).provider(userRequest.getProvider()).build();
             return userMapper.mapperToUserResponse(user);
         } catch (Exception ex) {
