341c177856d17da7106274e8ddbfa8470b6efc82
hoangminh-tran <90591751+hoangminh-tran@users.noreply.github.com>
2024-07-20 20:53:44 +0700

    Update Response in Material and BrandMaterial

--- src/main/java/com/smart/tailor/service/impl/MaterialServiceImpl.java
+++ src/main/java/com/smart/tailor/service/impl/MaterialServiceImpl.java
@@ -259,7 +259,7 @@ public class MaterialServiceImpl implements MaterialService {
             }
 
             if (!errorFields.isEmpty()) {
-                throw new ExcelFileInvalidDataTypeException("Some Data could not be processed correctly", errorFields);
+                throw new ExcelFileInvalidDataTypeException("The Material Excel File have " + materialRequests.size() + " create Success and " + errorFields.size() + " create Failure", errorFields);
             }
         } catch (IOException ex) {
             logger.error("Error processing excel file", ex);
