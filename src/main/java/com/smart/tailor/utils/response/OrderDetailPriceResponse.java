6cb7d4e4be2118c09ad73b731feb7e9243af9b21
hoangminh-tran <90591751+hoangminh-tran@users.noreply.github.com>
2024-08-01 20:59:39 +0700

    Update Code Calculate Price for Order

---
+++ src/main/java/com/smart/tailor/utils/response/OrderDetailPriceResponse.java
@@ -0,0 +1,24 @@
+package com.smart.tailor.utils.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDetailPriceResponse {
+    private Double totalPriceOfParentOrder;
+
+    private Double customerPriceDeposit;
+
+    private Double customerPriceFirstStage;
+
+    private Double customerSecondStage;
+
+    private List<BrandDetailPriceResponse> brandDetailPriceResponseList;
+}
