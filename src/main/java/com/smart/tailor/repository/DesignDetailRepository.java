b7bd9a95bfa9cc01b547f053db8f4f6ad6434079
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-08-05 12:23:55 +0700

    [BE0072] update merge order

--- src/main/java/com/smart/tailor/repository/DesignDetailRepository.java
+++ src/main/java/com/smart/tailor/repository/DesignDetailRepository.java
@@ -19,7 +19,7 @@ public interface DesignDetailRepository extends JpaRepository<DesignDetail, UUID
 
     DesignDetail findDesignDetailByDesignDesignIDAndSizeSizeID(UUID designID, UUID sizeID);
 
-    @Query(nativeQuery = true, value = "SELECT d.* FROM design_detail d JOIN orders o ON d.order_id = o.order_id WHERE o.order_type = 'SUB_ORDER' AND o.order_id = ?1 AND d.brand_id = ?2")
+    @Query(nativeQuery = true, value = "SELECT d.* FROM design_detail d JOIN orders o ON d.order_id = o.order_id WHERE o.order_type = 'SUB_ORDER' AND o.parent_order_id = ?1 AND d.brand_id = ?2")
     DesignDetail getDetailOfOrderBaseOnBrandID(UUID orderID, UUID brandID);
 
     Optional<DesignDetail> getDesignDetailByDesignDetailID(UUID designDetailID);
