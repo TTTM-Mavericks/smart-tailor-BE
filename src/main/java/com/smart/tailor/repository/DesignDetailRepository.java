53fbf417539b1b90e41b6977c720f20bbf25df44
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-08-06 08:35:44 +0700

    [BE0000] fix conflig

--- src/main/java/com/smart/tailor/repository/DesignDetailRepository.java
+++ src/main/java/com/smart/tailor/repository/DesignDetailRepository.java
@@ -19,7 +19,7 @@ public interface DesignDetailRepository extends JpaRepository<DesignDetail, Stri
 
     DesignDetail findDesignDetailByDesignDesignIDAndSizeSizeID(String designID, String sizeID);
 
-    @Query(nativeQuery = true, value = "SELECT d.* FROM design_detail d JOIN orders o ON d.order_id = o.order_id WHERE o.order_type = 'SUB_ORDER' AND o.order_id = ?1 AND d.brand_id = ?2")
+    @Query(nativeQuery = true, value = "SELECT d.* FROM design_detail d JOIN orders o ON d.order_id = o.order_id WHERE o.order_type = 'SUB_ORDER' AND o.parent_order_id = ?1 AND d.brand_id = ?2")
     DesignDetail getDetailOfOrderBaseOnBrandID(String orderID, String brandID);
 
     Optional<DesignDetail> getDesignDetailByDesignDetailID(String designDetailID);
