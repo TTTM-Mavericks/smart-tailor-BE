0e8b7c02fd8a686d219d32c103ea9cf5a99a1e77
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-08-09 17:25:56 +0700

    [BE0073] update notification feature

--- src/main/java/com/smart/tailor/config/WebSocketConfig.java
+++ src/main/java/com/smart/tailor/config/WebSocketConfig.java
@@ -7,8 +7,7 @@ import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(getUserHandShakeHandler(), "/websocket")
@@ -19,19 +18,5 @@ public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBro
     @Bean
     DataHandler getUserHandShakeHandler() {
         return new DataHandler();
-    }
-
-        @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("*")
-//                .setHandshakeHandler(new UserHandshakeHandler())
-                .withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/ws");
-        registry.enableSimpleBroker("/topic");
     }
 }
