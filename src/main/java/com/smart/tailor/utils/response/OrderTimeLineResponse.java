bed296781db806375d949bcef18790569f3914a6
hoangminh-tran <90591751+hoangminh-tran@users.noreply.github.com>
2024-08-13 18:52:36 +0700

    [BE0074] Create OrderTimeLine for SubOrder along with Customer Payment

--- src/main/java/com/smart/tailor/utils/response/OrderTimeLineResponse.java
+++ src/main/java/com/smart/tailor/utils/response/OrderTimeLineResponse.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class OrderTimeLineResponse {
+    private String estimatedDateStartDepositStage;
+
     private Integer estimatedQuantityFinishFirstStage;
 
     private String estimatedDateFinishFirstStage;
