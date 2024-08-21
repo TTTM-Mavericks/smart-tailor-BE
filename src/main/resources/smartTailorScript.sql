3d3192fd678e5b85d443991656ca44e72990ae32
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-08-21 18:30:20 +0700

    [BE0000] update script

--- src/main/resources/smartTailorScript.sql
+++ src/main/resources/smartTailorScript.sql
@@ -67,7 +67,7 @@ VALUES (GenerateCustomKeyString(), current_timestamp, null,
         (GenerateCustomKeyString(), current_timestamp, null, 'The province of Smart Tailor Company', 'SMART_TAILOR_PROVINCE',
         'STRING', true, 'PROVINCE', 'Hồ Chí Minh'),
          (GenerateCustomKeyString(), current_timestamp, null, 'The conversion ratio from pixel to centimeters', 'PIXEL_TO_CENTIMETER',
-        'DOUBLE', true, 'CENTIMETER', '0.02645833');
+        'DOUBLE', true, 'CENTIMETER', '0.02645833'),
        (GenerateCustomKeyString(), current_timestamp, null,
         '.', 'FEE_CANCEL_IN_DURATION', 'PERCENTAGE', true, 'PERCENT',
         '20');
